a80b0a6bc4a7b73124acbd7fce3d8e30da33d800
Senezaka <132164200+Senezaka@users.noreply.github.com>
2024-06-03 20:38:00 +0700

    Add files via upload

---
+++ Aplikasi_Penjualan_Sederhana.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi_penjualan_sederhana;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Aplikasi_Penjualan_Sederhana {
+    public static void main(String[] args){
+        koneksi k = new koneksi ();
+        k.connect();
+    }
+    
+}
